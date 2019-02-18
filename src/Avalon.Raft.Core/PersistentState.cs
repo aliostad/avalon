dee9a25a7bd8f4c80708604068539b2b74708ed9
Ali Kheyrollahi <aliostad@gmail.com>
2019-02-18 20:47:02 +0000

    wip

--- src/Avalon.Raft.Core/PersistentState.cs
+++ src/Avalon.Raft.Core/PersistentState.cs
@@ -12,7 +12,6 @@ namespace Avalon.Raft.Core
         public PersistentState()
         {
             Id = Guid.NewGuid();
-            LastVotedForId = new Guid();
             CurrentTerm = 0L;
         }
 
@@ -20,14 +19,14 @@ namespace Avalon.Raft.Core
 
         public long CurrentTerm { get; set; }
 
-        public Guid LastVotedForId { get; set; }
+        public Guid? LastVotedForId { get; set; }
 
         public byte[] ToBytes()
         {
             var bb = new List<byte>();
             bb.AddRange(Id.ToByteArray());
             bb.AddRange(BitConverter.GetBytes(CurrentTerm));
-            bb.AddRange(LastVotedForId.ToByteArray());
+            bb.AddRange(LastVotedForId.GetValueOrDefault().ToByteArray());
             return bb.ToArray();
         }
 
@@ -39,12 +38,23 @@ namespace Avalon.Raft.Core
             if (buffer.Length != 40)
                 throw new InvalidOperationException($"Buffer must be {BufferLength} but was {buffer.Length}");
 
-            return new PersistentState()
+            var state = new PersistentState()
             {
                 Id = new Guid(buffer.Take(16).ToArray()),
                 CurrentTerm = BitConverter.ToInt64(buffer, 16),
                 LastVotedForId = new Guid(buffer.Skip(16+8).ToArray())
             };
+
+            if (state.LastVotedForId == default(Guid))
+                state.LastVotedForId = null;
+
+            return state;
+        }
+
+        public void IncrementTerm()
+        {
+            CurrentTerm += 1;
+            LastVotedForId = null;
         }
     }
 }
---
+++ src/Avalon.Raft.Core/Rpc/DefaultRaftServer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avalon.Raft.Core.Rpc
+{
+    public class DefaultRaftServer : IRaftServer
+    {
+        protected PersistentState _persistentState = new PersistentState();
+        protected VolatileState _volatileState = new VolatileState();
+
+        public async Task<AppendEntriesResponse> AppendEntriesAsync(AppendEntriesRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<InstallUpdateResponse> InstallUpdateAsync(InstallUpdateRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RequestVoteResponse> RequestVoteAsync(RequestVoteRequest request)
+        {
+            if (_persistentState.CurrentTerm > request.CurrentTerm)
+                return Task.FromResult(new RequestVoteResponse()
+                {
+                    CurrentTrem = _persistentState.CurrentTerm,
+                    VoteGranted = false
+                });
+
+            if (!_persistentState.LastVotedForId.HasValue || _volatileState.CommitIndex <= request.LastLogIndex)
+                return Task.FromResult(new RequestVoteResponse()
+                {
+                    CurrentTrem = _persistentState.CurrentTerm,
+                    VoteGranted = true
+                });
+
+            // assume the rest we send back no
+            return Task.FromResult(new RequestVoteResponse()
+            {
+                CurrentTrem = _persistentState.CurrentTerm,
+                VoteGranted = false
+            });
+
+        }
+    }
+}
